6ae4eb5634305857c932557555c9779e816ab1ba
Erick Sorto <sortoerick33@gmail.com>
2023-10-17 19:30:56 -0300

    Optimize and add flows to CollatzCalculator.kt

--- app/src/main/java/com/ballisticapps/collatzConjectureVisualizer/data/CollatzCalculator.kt
+++ app/src/main/java/com/ballisticapps/collatzConjectureVisualizer/data/CollatzCalculator.kt
@@ -1,26 +1,44 @@
 package com.ballisticapps.collatzConjectureVisualizer.data
+import dagger.Module
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import java.math.BigInteger
+
+/**
+ * An implementation for computing the Collatz sequence for a given number.
+ *
+ * The Collatz sequence is a mathematical sequence where the next term is obtained from the
+ * previous term as follows: if the previous term is even, the next term is one half of the previous term.
+ * If the previous term is odd, the next term is 3 times the previous term plus 1.
+ * The sequence always ends with the numbers 4, 2, 1.
+ *
+ * @property oneBigInt Constant value representing the number 1 as a BigInteger.
+ * @property threeBigInt Constant value representing the number 3 as a BigInteger.
+ *
+ * @sample createCollatzList Uses the provided BigInteger value to generate and return the Collatz sequence.
+ */
+@Module
 class CollatzCalculator {
     private val oneBigInt = BigInteger("1")
     private val threeBigInt = BigInteger("3")
 
-    fun createCollatzList(numEntered: BigInteger): List<BigInteger> {
-        val collatzList = mutableListOf<BigInteger>()
+    /**
+     * Generates the Collatz sequence for the provided number using Flow.
+     *
+     * @param numEntered The starting number for which the Collatz sequence is to be computed.
+     * @return A flow emitting each number in the Collatz sequence starting from the provided number.
+     */
+    fun createCollatzList(numEntered: BigInteger): Flow<BigInteger> = flow {
         var currentNumber = numEntered
-
-        collatzList.add(currentNumber)
+        emit(currentNumber)
 
         while (currentNumber.bitLength() > 1) {
-            currentNumber = if (currentNumber.isOdd()) {
+            currentNumber = if (currentNumber.testBit(0)) {
                 currentNumber.multiply(threeBigInt).add(oneBigInt)
             } else {
                 currentNumber.shiftRight(1)
             }
-            collatzList.add(currentNumber)
+            emit(currentNumber)
         }
-
-        return collatzList
     }
-
-    private fun BigInteger.isOdd() = this.testBit(0)
 }
--- app/src/main/java/com/ballisticapps/collatzConjectureVisualizer/presentation/collatzCalculatorScreen/viewmodel/CollatzViewModel.kt
+++ app/src/main/java/com/ballisticapps/collatzConjectureVisualizer/presentation/collatzCalculatorScreen/viewmodel/CollatzViewModel.kt
@@ -1,19 +1,36 @@
 package com.ballisticapps.collatzConjectureVisualizer.presentation.collatzCalculatorScreen.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ballisticapps.collatzConjectureVisualizer.data.CollatzCalculator
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import java.math.BigInteger
+import javax.inject.Inject
 
 @HiltViewModel
-class CollatzViewModel() : ViewModel() {
+class CollatzViewModel @Inject constructor(
+    private val collatzCalculator: CollatzCalculator
+) : ViewModel() {
 
     private val _collatzViewModelState = MutableStateFlow(CollatzViewModelState())
     val collatzViewModelState: StateFlow<CollatzViewModelState> = _collatzViewModelState.asStateFlow()
+
+    fun computeCollatzSequence(numEntered: BigInteger) {
+        viewModelScope.launch {
+            collatzCalculator.createCollatzList(numEntered).collect { number ->
+                _collatzViewModelState.update { currentState ->
+                    currentState.copy(collatzSequenceList = currentState.collatzSequenceList + number)
+                }
+            }
+        }
+    }
 }
 
 data class CollatzViewModelState(
-    val collatzSequenceList: MutableList<BigInteger> = mutableListOf(),
+    val collatzSequenceList: List<BigInteger> = mutableListOf(),
 )
